e2af75eaf4e9c713daf8f03b1f2429f42466ac61
hamid3311khan-hub <hamid3311khan@gmail.com>
2026-06-14 15:16:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from db import get_db, init_app
+from flask import Flask, render_template
+from db import init_app
 from job_routes import job_bp
 import os
 
@@ -9,17 +9,18 @@ app.register_blueprint(job_bp)
 
 @app.route('/')
 def home():
-    return "SureJob is Live! Server Running ✅"
+    return render_template('index.html')
 
 @app.route('/db-test')
 def db_test():
+    from db import get_db
     try:
         db = get_db()
         cur = db.cursor()
         cur.execute('SELECT version();')
-        db_version = cur.fetchone()
+        version = cur.fetchone()
         cur.close()
-        return f"DB Connected: {db_version['version']}"
+        return f"DB Connected: {version['version']}"
     except Exception as e:
         return f"DB Error: {str(e)}", 500
 
